b9de093c7b4c1fbdb687a9b163f86a77ba31af89
Alexander Huber <alih83@gmx.de>
2015-03-31 21:16:34 +0200

    lowered unicorn processes

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
 listen 3000, tcp_nopush: false
 
 if ENV["RAILS_ENV"] == "development"
-  worker_processes 3
+  worker_processes 1
 else
-  worker_processes 3
+  worker_processes 1
 end
 
 CONN_SETTINGS = {
