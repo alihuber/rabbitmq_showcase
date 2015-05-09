2cefddd9e6ae968a7f23c54670696996a29471ef
Alexander Huber <alih83@gmx.de>
2015-05-09 16:55:32 +0200

    typo in logger output

--- app/workers/process_pdf_messages_worker.rb
+++ app/workers/process_pdf_messages_worker.rb
@@ -15,7 +15,7 @@ class ProcessPdfMessagesWorker
     file_name  = renderer.call(work[0], work[1])
     publish(file_name, to_queue: "pdfs_out")
 
-    logger.info("Finished rendering PDF #{work[0]}!'")
+    logger.info("Finished rendering PDF '#{work[0]}!'")
     ack!
   end
 end
